76fd494e25cd9a24462df92d30ae517c8a56b1da
Oliver Foster <oliver.foster@kineo.com>
2015-11-26 14:37:56 +0000

    Enabled disable

--- js/adapt-keepScrollPosition.js
+++ js/adapt-keepScrollPosition.js
@@ -9,13 +9,24 @@ define([ "coreJS/adapt" ], function(Adapt) {
 	var position = {};
 
 	function savePosition() {
+		if (!shouldRestorePosition()) return;
 		position[Adapt.location._currentId] = window.scrollY || window.pageYOffset;
 	}
 
 	function restorePosition() {
+		if (!shouldRestorePosition()) return;
 		var savedPosition = position[Adapt.location._currentId];
 
 		if (savedPosition) $(window).scrollTop(savedPosition);
+	}
+	
+	function shouldRestorePosition() {
+		try {
+			var model = Adapt.findById(Adapt.location._currentId);
+			var config = model.get("_keepScrollPosition");
+			if (config && config._isEnabled === false) return false;
+		} catch(e) {}
+		return true;
 	}
 
 	Adapt
